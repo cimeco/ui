bce140aa9dae2d6a1107be92ab11ef4c1055fe33
Franco2107 <fpautasso@lavozdelinterior.com.ar>
2022-03-11 15:41:08 -0300

    borrando section-separator

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import headline from "./headline";
+import carousel from "./carousel";
+import AuthorHeader from "./AuthorHeader";
+import sectionContainer from "./section-container";
+import timestamp from "./timestamp"
+
+export {headline, carousel, AuthorHeader, sectionContainer, timestamp}
